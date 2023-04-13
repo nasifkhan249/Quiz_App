19fb9a3907d25136fc419a779ed74e4e4312e996
Nasif Mustufa Khan <97841869+nasifkhan249@users.noreply.github.com>
2023-04-14 03:30:54 +0600

    add style and add js

---
+++ js/app.js
@@ -0,0 +1,64 @@
+let questions = [
+    {
+        number:1,
+        question:"What Does HTML Stand for ?",
+        answer:"Hyper Text Markup Language",
+        option:[
+
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ],
+    },
+
+    {
+        number:2,
+        question:"What Does Css Stand For ?",
+        answer:"Cascading Style Sheet",
+        option:[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+
+    {
+        number:3,
+        question:"What does PHP Stand For ?",
+        answer:"Hypertext Preprocessor",
+        option:[
+
+            "Hypertext Preprocessor",
+            "Hypertext Programing",
+            "Hypertext Preprograming",
+            "Hometext Preprograming",
+        ]
+    },
+
+    {
+        number:4,
+        question:"What does SQL Stand For ?",
+        answer:"Structured Query Language",
+        option:[
+
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language",
+        ]
+    },
+    {
+        number:5,
+        question:"What does XML Stand For ?",
+        answer:"eXtensible Markup Language",
+        option:[
+
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXtra Multi-program Language",
+            "eXamine Multiple Language",
+        ]
+    },
+]
--- js/index.js
+++ js/index.js
@@ -6,19 +6,58 @@ const exitButton = document.querySelector(".exitButton");
 
 const continueButton = document.querySelector(".continueButton");
 
-const questionBox = document.querySelector(".questionBox")
+const questionBox = document.querySelector(".questionBox");
 
 
 startBtn.onclick = () =>{
     rulesBox.classList.add("RulesBoxActive")
 }
 
-continueButton.onclick = () =>{
-    questionBox.classList.add("questionBoxActive")
-}
-
-
 exitButton.onclick = () =>{
     rulesBox.classList.remove("RulesBoxActive")
 }
+continueButton.onclick = () =>{
+    rulesBox.classList.remove("RulesBoxActive")
+    questionBox.classList.add("questionBoxActive")
+    showQuestion(0);
+}   
+
+const nextBtn = document.querySelector('.nextBtn');
+
+
+
+let totalCount = 0;
+
+nextBtn.onclick =  ()=>{
+    if(totalCount < questions.length -1){
+        totalCount++;
+        showQuestion(totalCount);
+    }
+}
+
+
+function showQuestion (index){
+    const Html = document.querySelector('.Html');
+    let HtmlTag = questions[index].number +'.'+ questions[index].question;
+    Html.innerHTML = HtmlTag;
+
+    const MyOptions = document.querySelector('.MyOptions');
+    let MyOptionsTag = '<div class="options"><span>'+questions[index].option[0]+'</span></div>'
+                        +'<div class="options"><span>'+questions[index].option[1]+'</span></div>'
+                        +'<div class="options"><span>'+questions[index].option[3]+'</span></div>'
+                        +'<div class="options"><span>'+questions[index].option[3]+'</span></div>'
+                        
+    MyOptions.innerHTML = MyOptionsTag;
+    const total_que = document.querySelector('.total_que');
+    let total_queTag = `${questions[index].number} of 5 Questions`;
+    total_que.innerHTML = total_queTag;
+}
+
+
+
+
+
+
+
+
 
